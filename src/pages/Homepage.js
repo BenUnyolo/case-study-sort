5d06a63912ba5660d318ac51e26f097fa4342750
Ben Unyolo <44584958+BenUnyolo@users.noreply.github.com>
2022-02-15 18:16:32 -0500

    styled and added logic for categories section

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,6 +8,7 @@ import "./Homepage.scss";
 function Homepage() {
   const [caseStudies, setCaseStudies] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     fetchCaseStudies();
@@ -40,10 +41,44 @@ function Homepage() {
   };
 
   return (
-    <div>
-      {categories.map((item) => {
-        return <span>{item.title}</span>;
-      })}
+    <div className="container">
+      <h1>Work</h1>
+      <div className="category-menu-container">
+        <div className="category-menu">
+          {/* all button */}
+          <div className="category-menu__btn-container">
+            <button
+              className={`category-menu__btn ${
+                selectedCategory === null ? "category-menu__btn--selected" : ""
+              }`}
+              aria-label={`show all case studies`}
+              onClick={() => setSelectedCategory(null)}
+            >
+              All
+            </button>
+          </div>
+          {/* category buttons */}
+          {categories.map((item) => {
+            const { title, slug } = item;
+            return (
+              <div key={slug} className="category-menu__btn-container">
+                <button
+                  className={`category-menu__btn ${
+                    slug === selectedCategory
+                      ? "category-menu__btn--selected"
+                      : ""
+                  }`}
+                  aria-label={`show only ${title} case studies`}
+                  onClick={() => setSelectedCategory(slug)}
+                >
+                  {title}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
       {caseStudies.map((item) => {
         return (
           <div key={item.id}>
